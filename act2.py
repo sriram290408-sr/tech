59fd4208a6898d54703657c8977f95b188804053
sriramesakkimuthu-art <sriram290408@gmail.com>
2025-10-23 09:01:45 +0530

    update

--- act2.py
+++ act2.py
@@ -1,23 +1,23 @@
 colors = ("red", "green", "blue")
 shapes = ("circle", "square", "triangle")
 
-# #1) Combine colors and shapes to create a new tuple called art.
+#1) Combine colors and shapes to create a new tuple called art.
 tuple_arts = (colors + shapes)
-# print(tuple_arts)
+print(tuple_arts)
 
-# #2) Demonstrate how to repeat a tuple, specifically colors three times.
-# tuple_arts = (colors* 3)
-# print(tuple_arts)
+#2) Demonstrate how to repeat a tuple, specifically colors three times.
+tuple_arts = (colors* 3)
+print(tuple_arts)
 
-# #3) Add an art element called “Diamond” to the art tuple.
-# tuple_arts += ("Diamond",)
-# print(tuple_arts)
+#3) Add an art element called “Diamond” to the art tuple.
+tuple_arts += ("Diamond",)
+print(tuple_arts)
 
-# #4) Extract and print the middle element from the art tuple using slicing.
-# middle = int(len(tuple_arts) / 2)
-# middle_element = tuple_arts[middle]
-# print(middle_element)
+#4) Extract and print the middle element from the art tuple using slicing.
+middle = int(len(tuple_arts) / 2)
+middle_element = tuple_arts[middle]
+print(middle_element)
 
-# #5) Verify if the string "square" exists within the art tuple.
-# check = "polygon" in tuple_arts
-# print(check)
+#5) Verify if the string "square" exists within the art tuple.
+check = "polygon" in tuple_arts
+print(check)
--- act3.py
+++ act3.py
@@ -1,16 +1,16 @@
 marks = (78, 85, 69, 90, 85)
 
-# #1) Determine the highest and lowest marks.
-# Highest_Mark = max(marks)
-# Lowest_Mark = min(marks)
+#1) Determine the highest and lowest marks.
+Highest_Mark = max(marks)
+Lowest_Mark = min(marks)
 
-# print("Highest Mark:", Highest_Mark)
-# print("Lowest Mark:", Lowest_Mark)
+print("Highest Mark:", Highest_Mark)
+print("Lowest Mark:", Lowest_Mark)
 
-# #2) Count the occurrences of the mark 85.
-# occurrence = marks.count(85)
-# print(occurrence)
+#2) Count the occurrences of the mark 85.
+occurrence = marks.count(85)
+print(occurrence)
 
-# #3) Calculate the average marks using the sum() and len() functions.
-# average = sum(marks)/ len(marks)
-# print(average)
+#3) Calculate the average marks using the sum() and len() functions.
+average = sum(marks)/ len(marks)
+print(average)
